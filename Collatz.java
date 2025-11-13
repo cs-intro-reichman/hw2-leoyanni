9a0800509eaeca94ad320d50c07d61794a45679f
Leo Yanni <leo.yanni@post.runi.ac.il>
2025-11-13 17:16:45 +0200

    Finished Homework 2

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,34 @@
-// Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);   
+        String mode = args[1];              
+
+        for (int seed = 1; seed <= N; seed++) {
+            int current = seed;
+            int steps = 1;  
+            if (mode.equals("v")) {
+                System.out.print(seed + " ");  
+
+            while (current != 1) {
+                if (current % 2 == 0) {
+                    current = current / 2;
+                } else {
+                    current = 3 * current + 1;
+                }
+
+                steps++;
+
+                if (mode.equals("v")) {
+                    System.out.print(current + " ");
+                }
+            }
+
+
+            if (mode.equals("v")) {
+                System.out.println("(" + steps + ")");
+            }
+        }
+
+        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+    }
 }
